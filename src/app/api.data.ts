576e041b8dc24bbfd9308b901a3ee7fe9e8c27d5
amrmustafa02 <amrmustafa6051@gmail.com>
2023-12-21 14:54:20 +0200

    edit in docker file

--- src/app/api.data.ts
+++ src/app/api.data.ts
@@ -1,6 +1,6 @@
 export class ApiData {
 
-  static baseUrl = "http://localhost:8000";
+  static baseUrl = "http://localhost:5000";
   static signUpEndPoint = "/auth/signUp";
   static loginEndPoint = "/auth/signin";
   static addSlotEndPoint = "/slot";
