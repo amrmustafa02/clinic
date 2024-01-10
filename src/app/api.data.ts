a6cbbce60277c8c80df0dc8818351afac80d503c
amr mustafa <78182930+amrmustafa02@users.noreply.github.com>
2024-01-10 17:15:27 +0200

    Update api.data.ts

--- src/app/api.data.ts
+++ src/app/api.data.ts
@@ -1,6 +1,6 @@
 export class ApiData {
 
-  static baseUrl = "http://localhost:5000";
+  static baseUrl = "https://clinicreservation.onrender.com";
   static signUpEndPoint = "/auth/signUp";
   static loginEndPoint = "/auth/signin";
   static addSlotEndPoint = "/slot";
